2015d61978e065e23201cee943beef4d13a07d14
kippy <amachorro02@gmail.com>
2024-05-06 15:44:33 -0700

    added temp static genre recommendations

--- app/components/GenreDoughnut.js
+++ app/components/GenreDoughnut.js
@@ -4,9 +4,12 @@ import { Doughnut } from 'react-chartjs-2';
 const GenreDoughnut = ({artists, genres}) => {
 
   const artistGenres = {}
+  const TEMP = {
+    genres: ["acoustic","afrobeat","alt-rock","alternative","ambient","anime","black-metal","bluegrass","blues","bossanova","brazil","breakbeat","british","cantopop","chicago-house","children","chill","classical","club","comedy","country","dance","dancehall","death-metal","deep-house","detroit-techno","disco","disney","drum-and-bass","dub","dubstep","edm","electro","electronic","emo","folk","forro","french","funk","garage","german","gospel","goth","grindcore","groove","grunge","guitar","happy","hard-rock","hardcore","hardstyle","heavy-metal","hip-hop","holidays","honky-tonk","house","idm","indian","indie","indie-pop","industrial","iranian","j-dance","j-idol","j-pop","j-rock","jazz","k-pop","kids","latin","latino","malay","mandopop","metal","metal-misc","metalcore","minimal-techno","movies","mpb","new-age","new-release","opera","pagode","party","philippines-opm","piano","pop","pop-film","post-dubstep","power-pop","progressive-house","psych-rock","punk","punk-rock","r-n-b","rainy-day","reggae","reggaeton","road-trip","rock","rock-n-roll","rockabilly","romance","sad","salsa","samba","sertanejo","show-tunes","singer-songwriter","ska","sleep","songwriter","soul","soundtracks","spanish","study","summer","swedish","synth-pop","tango","techno","trance","trip-hop","turkish","work-out","world-music"]
+  }
 
   function addGenres(artistGenres, genreName){
-    if (genres.genres.some(substring => genreName.includes(substring))){
+    if (TEMP.genres.some(substring => genreName.includes(substring))){
       if (artistGenres.hasOwnProperty(genreName)){
         artistGenres[genreName]++
       }
@@ -38,12 +41,12 @@ const GenreDoughnut = ({artists, genres}) => {
           label: 'Artists w/ Genre: ',
           data: Object.values(sortedGenres).slice(0, 10),
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.4)',
+            'rgba(54, 162, 235, 0.4)',
+            'rgba(255, 206, 86, 0.4)',
+            'rgba(75, 192, 192, 0.4)',
+            'rgba(153, 102, 255, 0.4)',
+            'rgba(255, 159, 64, 0.4)',
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
