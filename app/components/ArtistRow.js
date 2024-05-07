53f21da8777dc342e5f3137bc655d10f1089b028
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-07 01:09:47 -0700

    urls on artists and songs

--- app/components/ArtistRow.js
+++ app/components/ArtistRow.js
@@ -1,12 +1,14 @@
 const ArtistRow = ({artist, index}) => {
     return (
-        <div className="flex flex-row items-center justify-between p-4">
-            <div className="flex flex-row items-center">
-                <p className="text-lg font-bold">{index + 1}</p>
-                <img className="rounded-lg w-48 h-48 ml-4" src={artist.images[0].url} alt={artist.name} />
+        <a href={artist.external_urls.spotify}>
+            <div className="flex flex-row items-center justify-between p-4 hover:bg-base-300 rounded-lg transition-all">
+                <div className="flex flex-row items-center">
+                    <p className="text-lg font-bold">{index + 1}</p>
+                    <img className="rounded-lg w-48 h-48 ml-4" src={artist.images[0].url} alt={artist.name} />
+                </div>
+                <p className="ml-4">{artist.name}</p>
             </div>
-            <p className="ml-4">{artist.name}</p>
-        </div>
+        </a>
     )
 }
 
--- app/components/SongRow.js
+++ app/components/SongRow.js
@@ -1,16 +1,18 @@
 const SongRow = ({song, index}) => {
     if (!song) return null;
     return (
-        <div className="flex flex-row items-center justify-between p-4">
-            <div className="flex flex-row items-center">
-                <p className="text-lg font-bold">{index + 1}</p>
-                <img className="rounded-lg w-48 h-48 ml-4" src={song.album.images[0].url} alt={song.name} />
+        <a href={song.external_urls.spotify}>
+            <div className="flex flex-row items-center justify-between p-4 hover:bg-base-300 rounded-lg transition-all">
+                <div className="flex flex-row items-center">
+                    <p className="text-lg font-bold">{index + 1}</p>
+                    <img className="rounded-lg w-48 h-48 ml-4" src={song.album.images[0].url} alt={song.name} />
+                </div>
+                <div className="flex flex-col text-right">
+                    <p className="ml-4 max-w-sm">{song.name}</p>
+                    <p className="ml-4 text-gray-400">{song.artists[0].name}</p>
+                </div>
             </div>
-            <div className="flex flex-col text-right">
-                <p className="ml-4 max-w-sm">{song.name}</p>
-                <p className="ml-4 text-gray-400">{song.artists[0].name}</p>
-            </div>
-        </div>
+        </a>
     )
 }
 
